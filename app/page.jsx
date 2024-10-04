c642c60b693b3018785f084d953ec4c320561172
Souvik Mukherjee <mukherjeesouvik606@gmail.com>
2024-10-05 00:03:43 +0530

    footer base

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Footer from "@components/Footer";
 export default function Home() {
     return (
         <div className="">
+            <Navbar/>
             <Footer/>
         </div>
     );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import "@styles/navbar.css"
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+    return <nav class="navbar">
+        <div class="nav-div">
+            <div class="logo"><h2>CRAVYN</h2></div>
+            <div class="tabs">
+                <li><Link class="tab" href="#">Home</Link></li>
+                <li><Link class="tab" href="#">Menu</Link></li>
+                <li><Link class="tab" href="#">AboutUs</Link></li>
+                <li><Link class="tab" href="#">Help</Link></li>
+            </div>
+            <div class="search">
+                <div class="box">
+                    <input type="text" placeholder="Search......"></input>
+                    <li><Link class="search" href="#"><Image src="/assets/images/search.svg" width={30} height={30}></Image></Link></li>
+                </div>
+            </div>
+            <div class="cart">
+                <li><Link class="cart" href="#"><Image src="/assets/images/cart.png" width={30} height={30}></Image></Link></li>
+            </div>
+            <div class="buttons">
+                <button class="login">
+                    <Image src="/assets/images/login.svg" width={20} height={20}></Image>
+                    <Link href="#">Login</Link>
+                </button>
+                <button class="register">
+                    <Image src="/assets/images/register.png" width={20} height={20}></Image>
+                    <Link href="#">Register</Link>
+                </button>
+            </div>
+        </div>
+    </nav>;
 };
 
 export default Navbar;
