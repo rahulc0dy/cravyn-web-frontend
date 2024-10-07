c9665bea7be995f6c2a1dd94b2d738a79ce37cdc
Rahul Chakraborty <rahulchakraborty337@gmail.com>
2024-10-07 11:17:16 +0530

    Footer Design Update (#5)
    * Changed directory Structure
    
    * Added empty last line settings.json
    
    * Minor configuration changes
    
    * footer base
    
    * footer base
    
    * footer base
    
    * Fixed issues: font not loading, prettierignore added
    
    * footer base
    
    * merged branch master
    
    * Footer Design updated as Figma
    
    * PR template added
    
    * SVG and prettierrc update
    
    * EOF newline added
    
    ---------
    
    Co-authored-by: Souvik Mukherjee <mukherjeesouvik606@gmail.com>
    Co-authored-by: Mr. 17 <saptakmanna100@gmail.com>

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,3 @@
-import Navbar from "@components/Navbar";
-
 export default function Home() {
-  return (
-    <div className="">
-      <Navbar />
-    </div>
-  );
+  return <div className=""></div>;
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,82 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="nav-div">
+        <div className="logo">
+          <h2>CRAVYN</h2>
+        </div>
+        <div className="tabs">
+          <li>
+            <Link className="tab" href="#">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="tab" href="#">
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link className="tab" href="#">
+              AboutUs
+            </Link>
+          </li>
+          <li>
+            <Link className="tab" href="#">
+              Help
+            </Link>
+          </li>
+        </div>
+        <div className="search">
+          <div className="box">
+            <input type="text" placeholder="Search......"></input>
+            <li>
+              <Link className="search" href="#">
+                <Image
+                  src="/assets/images/search.svg"
+                  width={30}
+                  height={30}
+                ></Image>
+              </Link>
+            </li>
+          </div>
+        </div>
+        <div className="cart">
+          <li>
+            <Link className="cart" href="#">
+              <Image
+                src="/assets/images/cart.png"
+                width={30}
+                height={30}
+              ></Image>
+            </Link>
+          </li>
+        </div>
+        <div className="buttons">
+          <button className="login">
+            <Image
+              src="/assets/images/login.svg"
+              width={20}
+              height={20}
+            ></Image>
+            <Link href="#">Login</Link>
+          </button>
+          <button className="register">
+            <Image
+              src="/assets/images/register.png"
+              width={20}
+              height={20}
+            ></Image>
+            <Link href="#">Register</Link>
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
