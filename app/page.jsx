9bc480bf5cd9f316abc79ffd41588ba080d53361
Rahul Chakraborty <rahulchakraborty337@gmail.com>
2024-10-15 14:31:20 +0530

    Login and signup UI (#6)
    * Added empty last line settings.json
    
    * Minor configuration changes
    
    * footer base
    
    * footer base
    
    * footer base
    
    * Fixed issues: font not loading, prettierignore added
    
    * footer base
    
    * merged branch master
    
    * Fixed issues of font resource error, minor structure and file changes [Prettier and vs code configs]  (#3)
    
    * Changed directory Structure
    
    * Added empty last line settings.json
    
    * Minor configuration changes
    
    * Fixed issues: font not loading, prettierignore added
    
    * merged branch master
    
    * Footer Design updated as Figma
    
    * PR template added
    
    * SVG and prettierrc update
    
    * EOF newline added
    
    * Logo update
    
    * Footer breakpoints updated, Overflow fixed
    
    * Padding fix
    
    * svg fix
    
    * Homepage navbar -desktop
    
    * navbar design -non responsive
    
    * Login/Signup without animation
    
    * login ui update
    
    * Login-signup UI update
    
    * Update login heading
    
    ---------
    
    Co-authored-by: Souvik Mukherjee <mukherjeesouvik606@gmail.com>
    Co-authored-by: Mr. 17 <saptakmanna100@gmail.com>

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,15 @@
-import Navbar from "@components/Navbar";
+export const metadata = {
+  title: "Cravyn",
+  description:
+    "Cravyn is an online food ordering system that delivers your food fresh and warm to you withing minutes so your hunger no longer waits.",
+};
 
-export default function Home() {
-    return (
-        <div className="">
-            <Navbar />
-        </div>
-    );
+export default function HomePage() {
+  return (
+    <main className="w-full flex flex-col place-items-center">
+      <section className=" max-w-screen-2xl"></section>
+      <section className=" max-w-screen-2xl"></section>
+      <section className=" max-w-screen-2xl"></section>
+    </main>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,43 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "login-background": "url('/assets/images/colorful-stingrays.svg')",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          orange: "#ffbb25",
+          red: "#e63a3a",
+          grey: "#282723",
+        },
+        secondary: {
+          red: "#960a0a",
+        },
+        tertiary: {
+          yellow: "#ffedc0",
+          grey: "#232323",
+        },
+        accent: {
+          yellow: "#ffda7f",
+          "yellow-light": "#fff1bd",
+          "red-dark": "#df3d3d",
+        },
+        warning: {
+          "yellow-light": "#ffdd68",
+        },
+        danger: {
+          red: "#ff324b",
+          "red-dark": "#f80407",
+        },
+        grey: {
+          "light-1": "#dbdbdb",
+          "light-2": "#f6f6f6",
+          "light-3": "#c6c6c6",
+          medium: "#747474",
+          "dark-2": "#5b5b5b",
+          "dark-3": "#484848",
+          "dark-4": "#212121",
+        },
       },
     },
   },
