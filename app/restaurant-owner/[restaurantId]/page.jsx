77987ccaeb592dfbdbe9b74606cc05eefb7decea
rahulc0dy <rahulchakraborty337@gmail.com>
2024-11-25 21:47:14 +0530

    fix restaurant-owner stats

---
+++ app/restaurant-owner/[restaurantId]/page.jsx
@@ -0,0 +1,132 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { getRestaurantFoodSalesData } from "@services/restaurantOwnerServices";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
+
+const FoodSalesPage = () => {
+  const { restaurantId } = useParams();
+
+  const { data, isSuccess, isLoading, isError, error } = useQuery({
+    queryKey: ["foodSalesData", restaurantId],
+    queryFn: () => getRestaurantFoodSalesData(restaurantId),
+    refetchInterval: 100000,
+    refetchIntervalInBackground: true,
+  });
+
+  return isSuccess ? (
+    <main className="wrapper">
+      <div className="p-5 mx-auto lg:max-w-4xl border-8 border-accent-yellow-light my-4 mb-8 rounded-lg">
+        <h1 className="text-2xl font-bold">
+          {isSuccess && data.data.restaurant.name}
+        </h1>
+        <p>
+          {isSuccess &&
+            `${data.data.restaurant.street}, ${data.data.restaurant.city}, ${data.data.restaurant.pin_code}`}
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row justify-between lg:max-w-4xl mx-auto gap-8">
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="py-5 px-12 bg-accent-yellow-light rounded-lg w-full md:w-96"
+        >
+          <h4 className="text-3xl font-semibold text-primary-grey">
+            Total Sales
+          </h4>
+          <h5 className="text-lg font-light">
+            (For {isSuccess && data.data.restaurant.name})
+          </h5>
+          <p className="text-3xl font-bold text-secondary-orange tracking-wider py-2  ">
+            ₹{" "}
+            {isSuccess
+              ? data.data.foodSales.reduce(
+                  (accumulator, currentValue) =>
+                    accumulator + parseFloat(currentValue.sum),
+                  0
+                )
+              : "000"}
+          </p>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="py-5 px-12 bg-accent-yellow-light rounded-lg w-full md:w-96"
+        >
+          <h4 className="text-3xl font-semibold text-primary-grey">
+            Total Orders
+          </h4>
+          <h5 className="text-lg font-light">
+            (For {isSuccess && data.data.restaurant.name})
+          </h5>
+          <p className="text-3xl font-bold text-secondary-orange tracking-wider py-2  ">
+            {isSuccess
+              ? data.data.foodSales.reduce(
+                  (accumulator, currentValue) =>
+                    accumulator + parseFloat(currentValue.count),
+                  0
+                )
+              : "000"}
+          </p>
+        </motion.div>
+      </div>
+      <h2 className="text-center text-xl font-bold mt-5">Top Sellers</h2>
+      <div className="flex flex-wrap gap-5 lg:max-w-4xl mx-auto p-0 lg:p-5">
+        {isLoading ? (
+          <h1>Loading</h1>
+        ) : (
+          <table
+            className="table-auto w-full my-3 md:text-xl bg-amber-50 bg-opacity-20"
+            cellPadding={10}
+          >
+            <thead className="bg-accent-yellow-light">
+              <tr className="">
+                <th className="p-5 border-2 md:text-2xl border-yellow-200 hidden md:block">
+                  No.
+                </th>
+                <th className="p-5 border-2 md:text-2xl border-yellow-200">
+                  Food Item Name
+                </th>
+                <th className="p-5 border-2 md:text-2xl border-yellow-200">
+                  Order Count
+                </th>
+                <th className="p-5 border-2 md:text-2xl border-yellow-200">
+                  Total Sales
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.data.foodSales.map((food, index) => (
+                <tr key={food.item_id} className="table-row text-gray-700">
+                  <td className="text-right border-2 border-yellow-200 hidden md:block">
+                    {index + 1}
+                  </td>
+                  <td className="text-left border-2 border-yellow-200">
+                    {food.food_name}
+                  </td>
+                  <td className="text-center border-2 border-yellow-200">
+                    {food.count}
+                  </td>
+                  <td className="text-right px-4 border-2 border-yellow-200">
+                    {food.sum}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </main>
+  ) : (
+    <h1 className="text-2xl text-red-500 font-bold min-h-screen text-center">
+      {isLoading ? "Loading..." : isError ? error.message : "No data to show!"}
+    </h1>
+  );
+};
+
+export default FoodSalesPage;
--- components/restaurantOwner/RestaurantCard.jsx
+++ components/restaurantOwner/RestaurantCard.jsx
@@ -13,114 +13,113 @@ const RestaurantCard = ({
   verification,
 }) => {
   return (
-    <motion.div
-      initial={{
-        opacity: 0,
-        y: 100,
-      }}
-      whileInView={{
-        opacity: 1,
-        y: 0,
-        transition: { delay: 0.3 },
-      }}
-      viewport={{ once: true }}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.95 }}
-      className="m-5 rounded-xl shadow-xl bg-white text-left max-w-full relative"
-    >
-      <div className="relative object-cover w-full">
-        <Image
-          className="w-full max-h-40 object-cover rounded-t-xl"
-          src={image}
-          width={800}
-          height={240}
-          alt={name}
-        />
-      </div>
-
-      <div className="absolute rounded-full bg-white top-3 left-3">
-        {verification === "verified" ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="#00cd52"
-            className="size-6 "
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
-            />
-          </svg>
-        ) : verification === "pending" ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="#ed7e00"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="red"
-            className="size-6 "
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-        )}
-      </div>
-
-      <div className="px-5 py-4">
-        <h4 className="font-bold text-primary-orange text-3xl">{name}</h4>
-        <p className="font-base text-grey-medium text-lg mb-2 text-wrap min-h-12">
-          {address}
-        </p>
-        <h5 className="font-semibold text-xl text-primary-grey">
+    <Link href={sales > 0 ? `/restaurant-owner/${id}` : "#"}>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 100,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+          transition: { delay: 0.3 },
+        }}
+        viewport={{ once: true }}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        className="m-5 rounded-xl shadow-xl bg-white text-left max-w-full relative"
+      >
+        <div className="relative object-cover w-full">
           <Image
-            className="inline-block align-text-top"
-            src="/assets/icons/card-star.png"
-            width={25}
-            height={25}
-            alt="star"
-          />{" "}
-          {verification === "pending"
-            ? "Pending Verification"
-            : verification === "rejected"
-            ? "Cannot be rated"
-            : stars}
-        </h5>
-
-        <p className="font-medium text-lg">
-          Sales:{" "}
-          <span className="font-bold text-secondary-orange text-lg tracking-widest px-2">
-            ₹ {sales}
-          </span>
-        </p>
-        <p className="font-medium text-lg">
-          Orders / day:{" "}
-          <span className="font-bold text-secondary-green text-lg tracking-widest px-2">
-            {ordersPerDay}
-          </span>
-        </p>
-      </div>
-    </motion.div>
+            className="w-full max-h-40 object-cover rounded-t-xl"
+            src={image}
+            width={800}
+            height={240}
+            alt={name}
+          />
+        </div>
+        <div className="absolute rounded-full bg-white top-3 left-3">
+          {verification === "verified" ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#00cd52"
+              className="size-6 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+              />
+            </svg>
+          ) : verification === "pending" ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="#ed7e00"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="red"
+              className="size-6 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          )}
+        </div>
+        <div className="px-5 py-4">
+          <h2 className="font-bold text-primary-orange text-3xl">{name}</h2>
+          <p className="font-base text-grey-medium text-lg mb-2 text-wrap min-h-12">
+            {address}
+          </p>
+          <h5 className="font-semibold text-xl text-primary-grey">
+            <Image
+              className="inline-block align-text-top"
+              src="/assets/icons/card-star.png"
+              width={25}
+              height={25}
+              alt="star"
+            />{" "}
+            {verification === "pending"
+              ? "Pending Verification"
+              : verification === "rejected"
+              ? "Cannot be rated"
+              : stars}
+          </h5>
+          <p className="font-medium text-lg">
+            Sales:{" "}
+            <span className="font-bold text-secondary-orange text-lg tracking-widest px-2">
+              ₹ {sales}
+            </span>
+          </p>
+          <p className="font-medium text-lg">
+            Total orders:{" "}
+            <span className="font-bold text-secondary-green text-lg tracking-widest px-2">
+              {ordersPerDay}
+            </span>
+          </p>
+        </div>
+      </motion.div>
+    </Link>
   );
 };
 
