87164fcdb4d530b6ef4ce09c7ab3c8f284783e42
Rahul Chakraborty <rahulchakraborty337@gmail.com>
2025-02-10 09:29:52 +0530

    Integrate Sentry with Dynamic Configuration and GlobalError Component (#33)

--- instrumentation.js
+++ instrumentation.js
@@ -1,6 +1,16 @@
 import { SHORT_NAME } from "@lib/constants";
 import { registerOTel } from "@vercel/otel";
+import * as Sentry from "@sentry/nextjs";
 
-export function register() {
+export async function register() {
   registerOTel(SHORT_NAME || "Cravyn");
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("./sentry.server.config");
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    await import("./sentry.edge.config");
+  }
 }
+
+export const onRequestError = Sentry.captureRequestError;
